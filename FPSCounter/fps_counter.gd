83ce2fb9e0e1193f95c23ff7393bbdc1eefa0c24
APSY aka Kenan <kregez@outlook.com>
2024-12-30 15:47:57 +0100

    Update FPSLabel every second instead of every frame

--- FPSCounter/fps_counter.gd
+++ FPSCounter/fps_counter.gd
@@ -1,6 +1,10 @@
 extends Label
 
+var time_passed: float = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	text = "FPS: %d" % Engine.get_frames_per_second()
+	time_passed += delta
+	if time_passed >= 1.0:
+		time_passed = 0.0
+		text = "FPS: %d" % Engine.get_frames_per_second()
